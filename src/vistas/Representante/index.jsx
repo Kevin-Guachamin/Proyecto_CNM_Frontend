613379ddb6c3dc047f7eabe0ff2c61f6276067cf
dennisP <dennis.perez@epn.edu.ec>
2025-03-08 12:48:00 -0500

    Se agrega una tabla para visualizar los estudiantes a cargo del representante

--- src/vistas/Representante/index.jsx
+++ src/vistas/Representante/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from '../../layout/containers/Layout';
-import { Home, Users, Settings, ChevronLeft, ChevronRight, Sun, Moon } from "lucide-react";
+import Tabla from '../../components/Tabla';
+import { Home, Users, Settings, Eye, ChevronLeft, ChevronRight, Sun, Moon } from "lucide-react";
 import { div } from 'framer-motion/client';
 import Header from "../../components/Header";
 
@@ -8,6 +9,13 @@ import Header from "../../components/Header";
 function Index() {
   // Estado para almacenar la información del usuario conectado
   const [usuario, setUsuario] = useState(null);
+  const [datosEstudiante, setDatosEstudiante] = useState([]);
+  const columnas = ["Estudiante", "Curso", "Especialidad", "Acciones"];
+
+  const handleVerClick = (estudiante) => {
+    console.log("ver detalles de: ", estudiante);
+    // logica
+  }
 
   // Simulación de usuario ficticio mientras se conecta con el backend
   useEffect(() => {
@@ -22,6 +30,23 @@ function Index() {
 
     // Mientras no se conecte al backend, dejamos un usuario de prueba
     setUsuario({ nombre: "Maria Pérez", rol: "Representante" });
+    setDatosEstudiante([
+      {
+        Estudiante: "Jose Andres Perez Jimenez", 
+        Curso: "el curso", 
+        Especialidad: "la especialidad",
+        Acciones: (
+          <button
+            className = "btn btn-primary btn-sm"
+            onClick = {() => handleVerClick("Jose Andres Perez Jimenez")}
+          >
+           <Eye size={16} className="me-1"/>
+           Ver calificaciones
+          </button>
+        )
+      },
+      {Estudiante: "Ana Maria Perez Jimenez", Curso: "el curso", Especialidad: "la especialidad"}
+    ]);
   }, []);
     
     const modules = [
@@ -37,7 +62,10 @@ function Index() {
             </div>
 
             <Layout modules = {modules}>
-
+              <Tabla
+                columnas={columnas}
+                datos={datosEstudiante} 
+              />
             </Layout>
         </div>
     )
