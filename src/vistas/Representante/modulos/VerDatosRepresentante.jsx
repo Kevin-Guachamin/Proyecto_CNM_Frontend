115a007e08c4f21688c74ab7e233a41998d57385
dennisP <dennis.perez@epn.edu.ec>
2025-03-26 11:11:13 -0500

    Se agrega variable para controlar el modal y otra variable para navegar

--- src/vistas/Representante/modulos/VerDatosRepresentante.jsx
+++ src/vistas/Representante/modulos/VerDatosRepresentante.jsx
@@ -3,35 +3,41 @@ import axios from "axios";
 import ViewData from "../../Admin/Estudiantes/ViewData";
 import Header from "../../../components/Header";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const VerDatosRepresentante = () => {
-   const [representante, setRepresentante] = useState([]);
-   const location = useLocation();
-   const nroCedulaRepresentante = location.state?.nroCedula;
-   console.log(nroCedulaRepresentante);
+  const [representante, setRepresentante] = useState([]);
+  const location = useLocation();
+  const nroCedulaRepresentante = location.state?.nroCedula;
+  const [mostrarModal, setMostrarModal] = useState(true);
+  const navigate = useNavigate();
 
-   const cargarDatosRepresentante = async () => {
+  const cargarDatosRepresentante = async () => {
     try {
-        const respuesta = await axios.get(`http://localhost:8000/representante/obtener/${nroCedulaRepresentante}`)
-        setRepresentante(respuesta.data);        
+      const respuesta = await axios.get(`http://localhost:8000/representante/obtener/${nroCedulaRepresentante}`)
+      setRepresentante(respuesta.data);
     } catch (error) {
-       console.error('Error al obtener datos de representante: ', error);
+      console.error('Error al obtener datos de representante: ', error);
     }
-   } 
+  }
 
-   useEffect(() => {
+  const OnCancel = () => {
+    setMostrarModal(false); // Oculta el modal
+    navigate(-1); // regresa a la pagina anterior
+  }
+
+  useEffect(() => {
     cargarDatosRepresentante();
-   }, []);
+  }, []);
 
-   return(
+  return (
     <div>
-        <div className="container-fluid p-0">
-          {representante && <Header isAuthenticated={true} usuario={representante} />}
-        </div>
-        <ViewData entity={representante}></ViewData>
+      <div className="container-fluid p-0">
+        {representante && <Header isAuthenticated={true} usuario={representante} />}
+      </div>
+      <ViewData entity={representante} onCancel={OnCancel}></ViewData>
     </div>
-   );
+  );
 }
 
 export default VerDatosRepresentante;
