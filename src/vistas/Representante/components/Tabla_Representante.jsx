6f2dfc79937c0fdeebd1eafa4871099891e65b40
dennisP <dennis.perez@epn.edu.ec>
2025-03-26 11:03:12 -0500

    Se agrega el boton 'Volver a modulos'

--- src/vistas/Representante/components/Tabla_Representante.jsx
+++ src/vistas/Representante/components/Tabla_Representante.jsx
@@ -2,8 +2,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Tabla_Representante.css'
+import { useNavigate } from "react-router-dom";
 
 const Tabla = ({datos, isLoading, handleVerCalificaciones}) => {
+    const navigate = useNavigate();
+
     // Comprueba que los props sean arrays
     if(!Array.isArray(datos)) {
         return <div className="alert alert-danger"> Error: Los datos deben ser un array </div>
@@ -21,55 +24,66 @@ const Tabla = ({datos, isLoading, handleVerCalificaciones}) => {
         );
     }
 
+    const handleOnClick = () => {
+        navigate('/representante'); // Usa el navigate ya definido
+    }
+
     // Tabla de los estudiantes a cargo del REPRESENTANTE
    return(
-    <div className="tableresponsive mt-3">
-        <table className="table table-bordered table-striped estudiante-table">
-            <thead>
-                <tr className="bg-primary-subtle">
-                    <th scope = "col" className="text-center bg-primary-subtle"> Estudiante </th> 
-                    <th scope = "col" className="text-center bg-primary-subtle"> Jornada </th>
-                    <th scope = "col" className="text-center bg-primary-subtle"> Especialidad</th>
-                    <th scope = "col" className="text-center bg-primary-subtle"> Accion </th>
-                </tr>
-            </thead>
-            <tbody>
-                {datos.length > 0 ? 
-                    (
-                        datos.map((estudiante, i)=> (
-                            <tr key={i}>
-                                <td className="text-center"> 
-                                    {estudiante.primer_nombre ?? '-'}{' '}
-                                    {estudiante.segundo_nombre ?? '-'}{' '}
-                                    {estudiante.primer_apellido ?? '-'}{' '}
-                                    {estudiante.segundo_apellido ?? '-'}{' '}
-                                 </td>
-                                <td className="text-center"> {estudiante.jornada} </td>
-                                <td className="text-center"> {estudiante.especialidad} </td>
-                                <td className="text-center">
-                                    <i 
-                                        className="bi bi-card-checklist text-primary mx-2 fs-3"
-                                        onClick={() => handleVerCalificaciones(estudiante.nroCedula)}
-                                        title="Ver calificaciones"
-                                        style={{ cursor: 'pointer' }}
-                                    ></i>
-                                    <i 
-                                        className="bi bi-info-circle text-info mx-2 fs-3"
-                                        onClick={() => handleVerCalificaciones(estudiante.nroCedula)}
-                                        title="Ver información"
-                                        style={{ cursor: 'pointer' }}
-                                    ></i>
-                                </td>
-                            </tr>           
-                        ))
-                    ):(
-                        <tr>
-                            <td className="text-center"> No hay datos disponibles </td>
-                        </tr>
-                    )}  
-            </tbody>
-        </table> 
-    </div>
+    <div>
+        <div className="tableresponsive mt-3">
+            <table className="table table-bordered table-striped estudiante-table">
+                <thead>
+                    <tr className="bg-primary-subtle">
+                        <th scope="col" className="text-center bg-primary-subtle"> Estudiante </th>
+                        <th scope="col" className="text-center bg-primary-subtle"> Jornada </th>
+                        <th scope="col" className="text-center bg-primary-subtle"> Especialidad</th>
+                        <th scope="col" className="text-center bg-primary-subtle"> Accion </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {datos.length > 0 ?
+                        (
+                            datos.map((estudiante, i) => (
+                                <tr key={i}>
+                                    <td className="text-center">
+                                        {estudiante.primer_nombre ?? '-'}{' '}
+                                        {estudiante.segundo_nombre ?? '-'}{' '}
+                                        {estudiante.primer_apellido ?? '-'}{' '}
+                                        {estudiante.segundo_apellido ?? '-'}{' '}
+                                    </td>
+                                    <td className="text-center"> {estudiante.jornada} </td>
+                                    <td className="text-center"> {estudiante.especialidad} </td>
+                                    <td className="text-center">
+                                        <i
+                                            className="bi bi-card-checklist text-primary mx-2 fs-3"
+                                            onClick={() => handleVerCalificaciones(estudiante.nroCedula)}
+                                            title="Ver calificaciones"
+                                            style={{ cursor: 'pointer' }}
+                                        ></i>
+                                        <i
+                                            className="bi bi-info-circle text-info mx-2 fs-3"
+                                            onClick={() => handleVerCalificaciones(estudiante.nroCedula)}
+                                            title="Ver información"
+                                            style={{ cursor: 'pointer' }}
+                                        ></i>
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td className="text-center"> No hay datos disponibles </td>
+                            </tr>
+                        )}
+                </tbody>
+            </table>  
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+            <button className="btn btn-primary btn-sm mt-3 mx-3" style={{ width: 'fit-content' }} onClick={() => handleOnClick()}>
+                Volver a modulos
+            </button>
+        </div> 
+    </div> 
    ); 
 }
 
