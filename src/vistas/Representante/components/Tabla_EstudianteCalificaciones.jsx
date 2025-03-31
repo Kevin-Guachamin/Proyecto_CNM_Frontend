e77bd552e4674bec3fd880b7d84bc85ac51b621c
dennisP <dennis.perez@epn.edu.ec>
2025-03-31 15:51:34 -0500

    Se agrega tabla para ver las calificaciones de un estudiante a cargo del representante

---
+++ src/vistas/Representante/components/Tabla_EstudianteCalificaciones.jsx
@@ -0,0 +1,52 @@
+// Tabla para ver reporte de calificaciones de un estudiante
+import { Tabs, Tab, Container } from "react-bootstrap";
+
+
+const TablaEstudianteCalificaciones = () => {
+   return(
+       <div className="content-container">
+           <Container className="mt-4">
+               <div className="d-flex justify-content-between align-items-center mb-4">
+                   <h2 className="mb-0">Gestión de Calificaciones</h2>
+                   <div>
+                       <button
+                           className="btn btn-success me-2"
+                           //onClick={handleExportExcel}
+                           title="Exportar a Excel"
+                       >
+                           <i className="bi bi-file-earmark-excel-fill"></i>
+                       </button>
+
+                       <button
+                           className="btn btn-danger me-2"
+                           //onClick={handleExportPDF}
+                           title="Exportar a PDF"
+                       >
+                           <i className="bi bi-file-earmark-pdf-fill"></i>
+                       </button>
+                   </div>
+               </div>
+
+               {/* TABS PRINCIPALES */}
+               <Tabs defaultActiveKey="quimestre1" id="calificaciones-tabs" className="mb-3" fill>
+                   {/* QUIMESTRE 1 */}
+                   <Tab eventKey="quimestre1" title="Quimestre 1">
+                       
+                   </Tab>
+
+                   {/* QUIMESTRE 2 */}
+                   <Tab eventKey="quimestre2" title="Quimestre 2">
+                       
+                   </Tab>
+
+                   {/* NOTA FINAL */}
+                   <Tab eventKey="notaFinal" title="Nota Final">
+                       
+                   </Tab>
+               </Tabs>
+           </Container>
+       </div> 
+   );
+}
+
+export default TablaEstudianteCalificaciones;
