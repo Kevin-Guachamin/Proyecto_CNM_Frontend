c28049f3f987fd878602bb7312b1af69aca595d8
Bryan <bryanlug2014@gmail.com>
2025-04-21 12:15:31 -0500

    Ahora se puede editar los datos de un representante

--- src/vistas/Admin/Modules/Estudiantil/Estudiantes/ViewData.jsx
+++ src/vistas/Admin/Modules/Estudiantil/Estudiantes/ViewData.jsx
@@ -1,17 +1,64 @@
-import React from 'react';
-import Boton from '../../../../../components/Boton';
-import DatePicker from 'react-datepicker';
+import React, { useState, useEffect } from 'react';
 import '../../../Styles/CrearEntidad.css';
 import "react-datepicker/dist/react-datepicker.css";
 import InfoRepresentante from './InfoRepresentante';
 import InfoEstudiante from './InfoEstudiante';
+import axios from 'axios';
+import { ErrorMessage } from '../../../../../Utils/ErrorMesaje';
+import CrearRepresentante from '../Representantes/CrearRepresentante'
+import Swal from 'sweetalert2';
 
 function ViewData({ estudiante }) {
+  const API_URL = import.meta.env.VITE_URL_DEL_BACKEND;
+  const token = localStorage.getItem("token")
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [representante, setRepresentante] = useState("")
+
+  useEffect(() => {
+    const obtenerRepresentante = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/representante/obtener/${estudiante.nroCedula_representante}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setRepresentante(response.data)
+      } catch (error) {
+        ErrorMessage(error);
+      }
+    };
+
+    obtenerRepresentante();
+  }, []);
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  }
+  const handleActualizar = (newRepresentante) => {
+    axios.put(`${API_URL}/representante/editar/${representante.nroCedula}`, newRepresentante, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then(res => {
+        setIsModalOpen(false)
+        setRepresentante(res.data)
+        Swal.fire({
+          icon: "success",
+          title: "Actualización de datos exitosa",
+          iconColor: "#218838",
+          confirmButtonText: "Entendido",
+          confirmButtonColor: "#003F89",
+        });
+      })
+      .catch(err => {
+        ErrorMessage(err)
+      })
+  }
   return (
     <div className="contenedor-detalle-estudiante">
       <InfoEstudiante estudiante={estudiante} />
-      <InfoRepresentante nroCedula={estudiante.nroCedula_representante} />
-    </div>    
+      {isModalOpen && <CrearRepresentante onCancel={toggleModal} entityToUpdate={representante} onSave={handleActualizar} />}
+      <InfoRepresentante representante={representante} />
+      <div >
+        <button className='btn-buscar' onClick={toggleModal}>Actualizar representante</button>
+      </div>
+    </div>
   );
 }
 
