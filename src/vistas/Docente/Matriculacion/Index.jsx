9ed086ae00050875497cbf406a8e45bfca5c94f7
Bryan <bryanlug2014@gmail.com>
2026-01-13 14:18:04 -0500

    Se quita una protección a ruta

--- src/vistas/Docente/Matriculacion/Index.jsx
+++ src/vistas/Docente/Matriculacion/Index.jsx
@@ -4,17 +4,13 @@ import Layout from "../../../layout/Layout";
 import Busqueda from "./Busqueda";
 import { useNavigate } from "react-router-dom";
 import { getModulos,transformModulesForLayout} from "../../getModulos";
-import { useAuth } from "../../../Utils/useAuth";
+
 
 function Index() {
   // Protección de ruta
-  const auth = useAuth("Profesor");
   
-  // Si no está autenticado, no renderizar nada
-  if (!auth.isAuthenticated) {
-    return null;
-  }
-
+  
+ 
   const [usuario, setUsuario] = useState(null);
   const API_URL = import.meta.env.VITE_URL_DEL_BACKEND;
   const navigate = useNavigate()
